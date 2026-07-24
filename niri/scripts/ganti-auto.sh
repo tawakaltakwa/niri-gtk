00a94f2d55dfe5fc6fc057166227a4a1df1ab531
tawakaltakwa <hujanhalodo@gmail.com>
2026-07-24 11:59:41 +0700

    modified:   niri/scripts/ganti-auto.sh

--- niri/scripts/ganti-auto.sh
+++ niri/scripts/ganti-auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Waktu interval (dalam detik) - 5 menit = 300 detik
-INTERVAL=300
+# Waktu interval (dalam detik) - 30 menit = 1800 detik
+INTERVAL=1800
 
 # Loop tak terbatas
 while true; do
